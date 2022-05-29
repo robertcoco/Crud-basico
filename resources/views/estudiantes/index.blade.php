553fc1085a98a69129c0dbcc9a40b02e5ae586b6
angel <perdonholaperdon123@gmail.com>
2022-05-28 20:58:37 -0400

    fase de store and consult

--- resources/views/estudiantes/index.blade.php
+++ resources/views/estudiantes/index.blade.php
@@ -1,1 +1,22 @@
-Hola este es el index
+<table class="table">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>matricula</th>
+            <th>foto</th>
+            <th>carrera</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($estudiantes as $estudiante)
+        <tr>
+            <td> {{ $estudiante->id}} </td>
+            <td> {{ $estudiante->nombreCompleto}} </td>
+            <td> {{ $estudiante->matricula}} </td>
+            <td> {{ $estudiante->foto}} </td>
+            <td>{{ $estudiante->carrera }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
